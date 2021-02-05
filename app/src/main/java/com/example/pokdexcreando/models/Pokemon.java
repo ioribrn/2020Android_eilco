fb5667f89210f82924d0c2ae54ebbe732a0116ee
devops <devops@nobo.life>
2021-02-05 21:25:01 +0100

    add

---
+++ app/src/main/java/com/example/pokdexcreando/models/Pokemon.java
@@ -0,0 +1,36 @@
+package com.example.pokdexcreando.models;
+
+import androidx.room.Entity;
+
+@Entity(tableName = "Pokemon_table")
+public class Pokemon {
+
+    private int number;
+    private String name;
+    private String url;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getNumber() {
+        String[] urlPartes = url.split("/");
+        return Integer.parseInt(urlPartes[urlPartes.length - 1]);
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
